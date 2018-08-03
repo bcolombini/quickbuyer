51b20e0ac7677b6f45ad6440dc87801ac77d1007
Bruno Colombini | Stone <bruno.colombini@BRRIOMD011736.local>
2018-08-03 20:34:00 -0300

    analyzer

--- buyer.py
+++ buyer.py
@@ -13,40 +13,59 @@ if (config.get("TERMS", "USE") == "FALSE"):
 
 bit = Api(config.get("API", "key"), config.get("API", "secret"))
 
-myBalanceBtc = float(sys.argv[1])
+my_past = bit.get_markets()[0]
 
-coinMarketStart = raw_input("COIN INITIALS: ")
-tempo = time.time()
-coinMarket = coinMarketStart.upper() + "_BTC"
-priceActual = bit.get_market(coinMarket)[0]['LastPrice']
-priceFuture = priceActual * float(config.get("BUY", "FACTOR"))
-amount = myBalanceBtc / float(priceFuture)
-amount = amount - amount * 0.01
-priceSell = 0.0005 / amount
+while True:
+    print("=========START NEW VERIFICATION=========")
+    my_actual = bit.get_markets()[0]
+    for x in range(0,len(my_actual)):
+        if('/BTC' in my_past[x]['Label'] and my_actual[x]['Change']-my_past[x]['Change'] > 1):
+            print(my_actual[x]['Label'] + " - "+ str(my_actual[x]['Change']-my_past[x]['Change']) + "%")
+    my_past = my_actual      
+    
 
-# print(bit.buy_limit(coinMarket, amount, priceFuture))
-# print(bit.submit_trade(coinMarket, 'Buy',priceFuture,amount))
 
-print(
-    "[INFO] - " + str(datetime.datetime.now()) + " - " + str(time.time() - tempo) + " - BALANCE: " + str(myBalanceBtc))
-print(
-    "[INFO] - " + str(datetime.datetime.now()) + " - " + str(time.time() - tempo) + " - LAST PRICE: " + str(
-        priceActual))
-print("[INFO] - " + str(datetime.datetime.now()) + " - " + str(time.time() - tempo) + " - BUY ORDER PRICE: " + str(
-    priceFuture))
-print(
-    "[INFO] - " + str(datetime.datetime.now()) + " - " + str(time.time() - tempo) + " - AMOUNT TO BUY: " + str(amount))
-print(
-    "[INFO] - " + str(datetime.datetime.now()) + " - " + str(
-        time.time() - tempo) + " - SELL PRICE: " + '{0:.15f}'.format(
-        priceSell))
+        
 
-# bit.sell_limit(coinMarket, amount, priceActual * float(config.get("SELL", "PERCENT")))
-# bit.submit_trade(coinMarket, 'Sell', priceActual * float(config.get("SELL", "PERCENT"), amount))
 
-raw_input("PRESS [ENTER] TO CANCEL ORDER AND EMERGENCY SELL")
+# for a in bit.get_markets()[0]:
+#     if(a['Change'] > 60 and '/BTC' in a['Label']):
+#         print(a)
 
-orders = bit.get_openorders(coinMarket)[0]['result']
+# myBalanceBtc = float(sys.argv[1])
+
+# coinMarketStart = raw_input("COIN INITIALS: ")
+# tempo = time.time()
+# coinMarket = coinMarketStart.upper() + "_BTC"
+# priceActual = bit.get_market(coinMarket)[0]['LastPrice']
+# priceFuture = priceActual * float(config.get("BUY", "FACTOR"))
+# amount = myBalanceBtc / float(priceFuture)
+# amount = amount - amount * 0.01
+# priceSell = 0.0005 / amount
+
+# # print(bit.buy_limit(coinMarket, amount, priceFuture))
+# # print(bit.submit_trade(coinMarket, 'Buy',priceFuture,amount))
+
+# print(
+#     "[INFO] - " + str(datetime.datetime.now()) + " - " + str(time.time() - tempo) + " - BALANCE: " + str(myBalanceBtc))
+# print(
+#     "[INFO] - " + str(datetime.datetime.now()) + " - " + str(time.time() - tempo) + " - LAST PRICE: " + str(
+#         priceActual))
+# print("[INFO] - " + str(datetime.datetime.now()) + " - " + str(time.time() - tempo) + " - BUY ORDER PRICE: " + str(
+#     priceFuture))
+# print(
+#     "[INFO] - " + str(datetime.datetime.now()) + " - " + str(time.time() - tempo) + " - AMOUNT TO BUY: " + str(amount))
+# print(
+#     "[INFO] - " + str(datetime.datetime.now()) + " - " + str(
+#         time.time() - tempo) + " - SELL PRICE: " + '{0:.15f}'.format(
+#         priceSell))
+
+# # bit.sell_limit(coinMarket, amount, priceActual * float(config.get("SELL", "PERCENT")))
+# # bit.submit_trade(coinMarket, 'Sell', priceActual * float(config.get("SELL", "PERCENT"), amount))
+
+# raw_input("PRESS [ENTER] TO CANCEL ORDER AND EMERGENCY SELL")
+
+# orders = bit.get_openorders(coinMarket)[0]['result']
 # for cancelOrder in orders:
 #     uuid = cancelOrder['OrderUuid']
 #     bit.cancel(uuid)
